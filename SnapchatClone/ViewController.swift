f825b9587cf27b6327109076a7564ec5b4407117
Neethu <neethuk109@gmail.com>
2022-08-08 15:04:05 -0400

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Neethu Kuruvilla on 8/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
